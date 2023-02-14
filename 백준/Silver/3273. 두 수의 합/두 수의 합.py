cd25d31e364122b4f3a97644a27a8cbfdcfaeb81
박민규 <73567364+mg4432@users.noreply.github.com>
2023-02-14 16:22:08 +0900

    [Silver III] Title: 두 수의 합, Time: 100 ms, Memory: 42172 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -0,0 +1,19 @@
+n = int(input()) 
+lst = list(map(int, input().split()))
+lst.sort()
+t = int(input())
+answer = 0
+
+l, r = 0, n-1
+while l < r :
+    Sum = lst[l] + lst[r]
+    if Sum < t : 
+        l += 1 
+    elif Sum > t : 
+        r -= 1 
+    else : 
+        answer += 1
+        r -= 1
+print(answer)
+
+    
