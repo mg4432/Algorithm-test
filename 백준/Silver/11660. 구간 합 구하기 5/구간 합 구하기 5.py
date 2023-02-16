cb2ae294077ea17ff8d26031058099ed79751fe9
박민규 <73567364+mg4432@users.noreply.github.com>
2023-02-16 17:39:58 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1000 ms, Memory: 73084 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,14 +1,22 @@
 import sys 
-n, m = map(int, sys.stdin.readline().strip().split()) 
+n, m = map(int, input().split()) 
 
-graph = [list(map(int, input().split())) for _ in range(n)]
-dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
 
 for i in range(1, n+1) : 
-    for j in range(1, n+1) : 
-        dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + graph[i-1][j-1]
+    graph[i] = [0] + list(map(int, sys.stdin.readline().split())) 
+
+
+for i in range(1, n+1) : 
+    graph[1][i] += graph[1][i-1]
+    graph[i][1] += graph[i-1][1]
+
+
+for i in range(2, n+1) : 
+    for j in range(2, n+1) : 
+        graph[i][j] += graph[i][j-1] + graph[i-1][j] - graph[i-1][j-1]
 
 for _ in range(m) : 
-    x1, y1, x2, y2 = map(int, sys.stdin.readline().strip().split()) 
-    answer = dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split()) 
+    answer = graph[x2][y2] - graph[x1-1][y2] - graph[x2][y1-1] + graph[x1-1][y1-1]
     print(answer)
