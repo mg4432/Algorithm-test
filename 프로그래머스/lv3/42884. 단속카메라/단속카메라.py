487af28e2a51f1cf0504351025cf9ec6dd9e9502
박민규 <73567364+mg4432@users.noreply.github.com>
2023-02-21 21:05:41 +0900

    [level 3] Title: 단속카메라, Time: 1.04 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,10 @@
+def solution(routes):
+    answer = 0
+    routes.sort(key = lambda x : x[1])
+    cam = -30001 
+    
+    for route in routes : 
+        if cam < route[0] : 
+            answer += 1 
+            cam = route[1]
+    return answer
