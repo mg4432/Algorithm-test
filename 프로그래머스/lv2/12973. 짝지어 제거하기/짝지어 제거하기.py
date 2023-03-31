a14acf77f39dcfd63d6341241c4ce111e34080d6
박민규 <73567364+mg4432@users.noreply.github.com>
2023-04-01 00:58:56 +0900

    [level 2] Title: 짝지어 제거하기, Time: 98.55 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,7 +1,7 @@
 def solution(s):
     stack = [] 
     
-    for letter in s : 
+    for letter in s :
         if not stack :
             stack.append(letter)
         else : 
@@ -9,5 +9,5 @@ def solution(s):
                 stack.pop()
             else : 
                 stack.append(letter)
-                
+        
     return 0 if stack else 1
