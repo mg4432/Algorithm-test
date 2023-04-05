64703e29489b079e521af824db0261ca1cd93089
박민규 <73567364+mg4432@users.noreply.github.com>
2023-04-05 20:16:25 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 848 ms, Memory: 39504 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -5,7 +5,7 @@ for i in range(2, int(m**0.5)+1) :
     sq = i ** 2 
     min_times = divmod(n-1, sq)[0] + 1
     for j in range(min_times * sq, m+1, sq) : 
-        if n <= j <= m and lst[j-n] == 1 : 
+        if  lst[j-n] == 1 : 
             lst[j-n] = 0
 
 print(sum(lst))
